6e0cc038088af79faa85ad2c6645455ec7355b67
aszadzinski <albert.szadzinski@us.edu.pl>
2022-10-13 11:38:48 +0200

    WdI: Z12

---
+++ Wstęp_do_informatyki/Zastępstwo_6-7.12.22/grupa_wtorek/func.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void f(int x) 
+{
+		printf("x= %d\n", x);
+}
+
+/*
+ void - funkcja która nic nie zwraca, gdy chcemy zwrócić wartość, najpierw deklarujemy typ np.:
+ int f(int x, int y)
+ {
+		return x+y;
+ }
+ */
