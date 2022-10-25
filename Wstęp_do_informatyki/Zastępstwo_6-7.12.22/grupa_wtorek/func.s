18f24e35eb2d0088cf5da00e47cef2756c73812a
aszadzinski <albert.szadzinski@us.edu.pl>
2022-10-25 11:48:57 +0200

    WdI: Z14 asm

---
+++ Wstęp_do_informatyki/Zastępstwo_6-7.12.22/grupa_wtorek/func.s
@@ -0,0 +1,33 @@
+	.file	"func.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"x= %d\n"
+	.text
+	.globl	f
+	.type	f, @function
+f:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	%edi, -4(%rbp)
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	f, .-f
+	.ident	"GCC: (GNU) 12.2.0"
+	.section	.note.GNU-stack,"",@progbits
---
+++ Wstęp_do_informatyki/Zastępstwo_6-7.12.22/grupa_wtorek/main.s
@@ -0,0 +1,36 @@
+	.file	"main.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"a= %d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$1, -8(%rbp)
+	movl	$1, -4(%rbp)
+	movl	-8(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$99, %edi
+	call	f@PLT
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 12.2.0"
+	.section	.note.GNU-stack,"",@progbits
